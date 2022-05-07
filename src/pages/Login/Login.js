f4f92fd5ff93c567408531383ae75c05ec0b431a
mdaias <mdaias29@gmail.com>
2022-05-08 01:18:20 +0600

    login page and registration page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import "./Login.css";
 
 const Login = () => {
+    // create email pass reference
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email,password)
+    }
+
     return (
-        <div>
-            
+        <div className='login-page bg-red-100 mx-auto container rounded-lg border-4 p-2'>
+            <h1 className='text-center text-blue-500 font-bold'>LogIn</h1>
+
+            <Form onSubmit={handleSubmit} className='mx-5 mb-3'>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className='font-semibold fs-4 text-sky-500'>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className='font-semibold fs-4 text-sky-500'>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+
+                <Button className='mt-2 w-28 font-extrabold text-2xl' variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <p className='text-center'>New to genius Car Service? <span className='cursor-pointer text-orange-600' onClick={()=>navigate('/registration')}>Registration</span></p>
         </div>
     );
 };
--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -3,8 +3,21 @@ import "./Registration.css";
 
 const Registration = () => {
     return (
-        <div>
-            <h1>registration</h1>
+        <div className='register-form container bg-zinc-50'>
+
+            <h1 className=' text-center text-emerald-500'>Registration</h1>
+
+            <form className=' mt-10'>
+                <input type="text" name='name' placeholder='Enter your full-name' />
+
+                <input type="email" name="email" placeholder='Enter a valid email address' id="" />
+
+                <input type="password" name="password" placeholder='Password' id="" />
+                <input type="password" name="confirmPassword" placeholder='Confirm Password' id="" />
+
+                <input className='w-50 mx-auto text-white bg-blue-300' type="submit" value="Registration" />
+            </form>
+
         </div>
     );
 };
