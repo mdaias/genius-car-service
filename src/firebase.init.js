201cb09d460ade8438f898910a641cc93c830710
mdaias <mdaias29@gmail.com>
2022-05-08 22:05:19 +0600

    set env variable for firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkJi5zuMc2RTIrlw4MBOk7neILQfovNis",
-  authDomain: "genius-car-service-20215.firebaseapp.com",
-  projectId: "genius-car-service-20215",
-  storageBucket: "genius-car-service-20215.appspot.com",
-  messagingSenderId: "415508241387",
-  appId: "1:415508241387:web:07345994bb36941dc617ae"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Registration.css";
 
 const Registration = () => {
+
+    const navigate = useNavigate();
+
+    const handleRegister = event => {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        console.log(name, email, password, confirmPassword)
+    }
+
+
     return (
         <div className='register-form container bg-zinc-50'>
 
             <h1 className=' text-center text-emerald-500'>Registration</h1>
 
-            <form className=' mt-10'>
+            <form onSubmit={handleRegister} className=' mt-10'>
                 <input type="text" name='name' placeholder='Enter your full-name' />
 
                 <input type="email" name="email" placeholder='Enter a valid email address' id="" />
@@ -17,6 +33,8 @@ const Registration = () => {
 
                 <input className='w-50 mx-auto text-white bg-blue-300' type="submit" value="Registration" />
             </form>
+
+            <p className='text-center'>Already have an account?<span className='cursor-pointer font-bold text-orange-600' onClick={() => navigate('/login')}> Login</span></p>
 
         </div>
     );
